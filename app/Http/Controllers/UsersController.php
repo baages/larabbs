65fa181927ba55db5ca6a55aae397c0ed971b23a
song_zhiobo <song_zhibo@dahuatech.com>
2020-10-21 21:34:10 +0800

    上传头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Handlers\ImageUploadHandler;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
@@ -18,9 +19,21 @@ class UsersController extends Controller
         return view('users.edit', compact('user'));
     }
 
-    public function update(UserRequest $request, User $user)
+    public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
         $user->update($request->all());
+
+        if ($request->avatar)
+        {
+            $result = $uploader->save($request->avatar, 'avatars', $user->id);
+
+            if ($request)
+            {
+                $data['avatar'] = $result['path'];
+            }
+        }
+
+        $user->update($data);
 
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功! ');
     }
