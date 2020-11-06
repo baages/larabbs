d05ac27a02d585253412bb7c5b81857874d24a58
song_zhiobo <song_zhibo@dahuatech.com>
2020-11-07 01:09:49 +0800

    翻译队列化

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
 use App\Models\Topic;
 
 // creating, created, updating, updated, saving,
@@ -18,10 +19,17 @@ class TopicObserver
         // 生活话题摘录
         $topic->excerpt = make_excerpt($topic->body);
 
+
+    }
+
+    public function saved(Topic $topic)
+    {
         // 如果slug字段无内容, 即使用翻译器对title进行翻译
         if ( ! $topic->slug)
         {
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+            // 推送任务到队列
+            dispatch(New TranslateSlug($topic));
+//            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
         }
     }
 }
