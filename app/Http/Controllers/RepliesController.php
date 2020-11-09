1915eaebbc52f271593bfd3e6eaf97257d4b2c84
song_zhiobo <song_zhibo@dahuatech.com>
2020-11-09 22:33:22 +0800

    消息通知有新回复

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -13,24 +13,7 @@ class RepliesController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
-//        $this->middleware('auth', ['except' => ['index', 'show']]);
     }
-
-//	public function index()
-//	{
-//		$replies = Reply::paginate();
-//		return view('replies.index', compact('replies'));
-//	}
-
-//    public function show(Reply $reply)
-//    {
-//        return view('replies.show', compact('reply'));
-//    }
-//
-//	public function create(Reply $reply)
-//	{
-//		return view('replies.create_and_edit', compact('reply'));
-//	}
 
 	public function store(ReplyRequest $request, Reply $reply)
 	{
@@ -41,23 +24,7 @@ class RepliesController extends Controller
 
 	    return redirect()->to($reply->topic->link())->with('success', '评论创建成功 !');
 
-//		$reply = Reply::create($request->all());
-//		return redirect()->route('replies.show', $reply->id)->with('message', 'Created successfully.');
 	}
-
-//	public function edit(Reply $reply)
-//	{
-//        $this->authorize('update', $reply);
-//		return view('replies.create_and_edit', compact('reply'));
-//	}
-//
-//	public function update(ReplyRequest $request, Reply $reply)
-//	{
-//		$this->authorize('update', $reply);
-//		$reply->update($request->all());
-//
-//		return redirect()->route('replies.show', $reply->id)->with('message', 'Updated successfully.');
-//	}
 
 	public function destroy(Reply $reply)
 	{
