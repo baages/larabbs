6ab8f6616915db56fc476e53eaa1b57d006dfabd
song_zhiobo <song_zhibo@dahuatech.com>
2020-11-11 22:14:09 +0800

    首页话题列表

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,8 @@ use Illuminate\Support\Facades\Route;
 //    return view('welcome');
 //});
 
-Route::get('/', 'PagesController@root')->name('root');
+//Route::get('/', 'PagesController@root')->name('root');
+Route::get('/', 'TopicsController@index')->name('root');
 
 // 用户身份验证相关的路由
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
@@ -53,4 +54,4 @@ Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]
 
 Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
 
-Route::get('permission-denied', 'PagesCOntroller@permissionDenied')->name('permission_denied');
+Route::get('permission-denied', 'PagesController@permissionDenied')->name('permission_denied');
