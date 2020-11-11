6596e98fe8af427daf8d2bef1b9d49b0dee8fe48
song_zhiobo <song_zhibo@dahuatech.com>
2020-11-11 17:13:20 +0800

    站点设置

--- config/administrator.php
+++ config/administrator.php
@@ -69,7 +69,10 @@ return array(
             'categories',
             'topics',
             'replies',
-        ]
+        ],
+        '站点管理' => [
+            'settings.site',
+        ],
     ],
 
     /*
